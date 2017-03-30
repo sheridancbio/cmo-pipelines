0aeb8996ac7ae65848c91ba675c4c15543c94387
Angelica Ochoa <aochoa4230@gmail.com>
2017-03-30 17:18:59 -0400

    Updated importer scripts
    Importer scripts now check db version before trying to import any data. Sends email if not compatible.
    
    Signed-off-by: Angelica Ochoa <aochoa4230@gmail.com>

--- import-scripts/import-cmo-data-msk.sh
+++ import-scripts/import-cmo-data-msk.sh
@@ -6,29 +6,50 @@ tmp=$PORTAL_HOME/tmp/import-cron-cmo-msk
 if [[ -d "$tmp" && "$tmp" != "/" ]] ; then
 	rm -rf "$tmp"/*
 fi
-
+email_list="heinsz@mskcc.org, sheridar@mskcc.org, grossb1@mskcc.org, ochoaa@mskcc.org, wilsonm2@mskcc.org"
 now=$(date "+%Y-%m-%d-%H-%M-%S")
 msk_automation_notification_file=$(mktemp $tmp/msk-automation-portal-update-notification.$now.XXXXXX)
 
-# import vetted studies into MSK portal
-echo "importing cancer type updates into msk portal database..."
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --import-types-of-cancer
-echo "importing study data into msk portal database..."
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal msk-automation-portal --update-worksheet --notification-file $msk_automation_notification_file --use-never-import
-num_studies_updated=`cat $tmp/num_studies_updated.txt`
-
-# redeploy war
-if [ $num_studies_updated -gt 0 ]
+DB_VERSION_FAIL=0
+# check database version before importing anything
+echo "Checking if database version is compatible"
+$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --check-db-version
+if [ $? -gt 0 ]
 then
-	echo "'$num_studies_updated' studies have been updated, requesting redeployment of msk portal war..."
-	ssh -i $HOME/.ssh/id_rsa_tomcat_restarts_key cbioportal_importer@dashi.cbio.mskcc.org touch /srv/data/portal-cron/schultz-tomcat-restart
-	#echo "'$num_studies_updated' studies have been updated (no longer need to restart schultz-tomcat server...)"
-else
-	#echo "No studies have been updated, skipping redeploy of msk portal war..."
-	echo "No studies have been updated.."
+    echo "Database version expected by portal does not match version in database!"
+    DB_VERSION_FAIL=1
 fi
 
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal msk-automation-portal --notification-file $msk_automation_notification_file
+if [ $DB_VERSION_FAIL -eq 0 ]
+then
+    # import vetted studies into MSK portal
+    echo "importing cancer type updates into msk portal database..."
+    $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --import-types-of-cancer
+    echo "importing study data into msk portal database..."
+    $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal msk-automation-portal --update-worksheet --notification-file "$msk_automation_notification_file" --use-never-import
+    num_studies_updated=`cat $tmp/num_studies_updated.txt`
+
+    # redeploy war
+    if [ $num_studies_updated -gt 0 ]
+    then
+	    echo "'$num_studies_updated' studies have been updated, requesting redeployment of msk portal war..."
+    	ssh -i $HOME/.ssh/id_rsa_tomcat_restarts_key cbioportal_importer@dashi.cbio.mskcc.org touch /srv/data/portal-cron/schultz-tomcat-restart
+	    echo "'$num_studies_updated' studies have been updated (no longer need to restart schultz-tomcat server...)"
+    else
+	    #echo "No studies have been updated, skipping redeploy of msk portal war..."
+    	echo "No studies have been updated.."
+    fi
+fi
+
+EMAIL_BODY="The GDAC database version is incompatible. Imports will be skipped until database is updated."
+# send email if db version isn't compatible
+if [ $DB_VERSION_FAIL -gt 0 ]
+then
+    echo -e "Sending email $EMAIL_BODY"
+    echo -e "$EMAIL_BODY" | mail -s "GDAC Update Failure: DB version is incompatible" $email_list
+fi
+
+$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27184 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-cmo-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal msk-automation-portal --notification-file "$msk_automation_notification_file"
 
 if [[ -d "$tmp" && "$tmp" != "/" ]] ; then
 	rm -rf "$tmp"/*
--- import-scripts/import-dmp-impact-data.sh
+++ import-scripts/import-dmp-impact-data.sh
@@ -5,6 +5,7 @@
 echo $(date)
 tmp=$PORTAL_HOME/tmp/import-cron-dmp-msk
 num_studies_updated=0
+email_list="heinsz@mskcc.org, sheridar@mskcc.org, grossb1@mskcc.org, ochoaa@mskcc.org, wilsonm2@mskcc.org"
 
 if [[ -d "$tmp" && "$tmp" != "/" ]]; then
 	rm -rf "$tmp"/*
@@ -12,7 +13,7 @@ fi
 
 now=$(date "+%Y-%m-%d-%H-%M-%S")
 mskimpact_notification_file=$(mktemp $tmp/mskimpact-portal-update-notification.$now.XXXXXX)
-mskheme_notification_file=$(mktemp $tmp/mskheme-portal-update-notification.$now.XXXXXX)
+#mskheme_notification_file=$(mktemp $tmp/mskheme-portal-update-notification.$now.XXXXXX)
 mskraindance_notification_file=$(mktemp $tmp/mskraindance-portal-update-notification.$now.XXXXXX)
 #mskarcher_notification_file=$(mktemp $tmp/mskarcher-portal-update-notification.$now.XXXXXX)
 
@@ -47,6 +48,7 @@ echo "fetching Darwin data"
 $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/darwin_fetcher.jar -d $MSK_IMPACT_DATA_HOME -s mskimpact
 cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest MSK-IMPACT Dataset: Darwin"
 
+DB_VERSION_FAIL=0
 IMPORT_STATUS=0
 IMPORT_FAIL=0
 VALIDATION_FAIL=0
@@ -54,6 +56,7 @@ DELETE_FAIL=0
 RENAME_BACKUP_FAIL=0
 RENAME_FAIL=0
 GROUPS_FAIL=0
+SUCCESS=0
 
 # fetch new/updated IMPACT samples using CVR Web service   (must come after mercurial fetching) 
 echo "fetching samples from CVR Web service  ..."
@@ -61,9 +64,10 @@ $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,add
 if [ $? -gt 0 ]
 then
 	echo "CVR fetch failed!"
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all;rm *.orig
+	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all --no-backup;rm *.orig
 	IMPORT_STATUS=1
 else
+	echo "committing cvr data"
 	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest MSK-IMPACT Dataset: CVR"
 fi
 
@@ -73,9 +77,10 @@ $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,add
 if [ $? -gt 0 ]
 then
 	echo "CVR Germline fetch failed!"
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all;rm *.orig
+	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all --no-backup;rm *.orig
 	IMPORT_STATUS=1
 else
+	echo "committing CVR germline data"
 	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest MSK-IMPACT Dataset: CVR Germline"
 fi
 
@@ -85,34 +90,35 @@ if [ $? -gt 0 ]
 then
 	echo "CVR raindance fetch failed!"
 	echo "This will not affect importing of mskimpact"
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all;rm *.orig
+	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all --no-backup;rm *.orig
 else
 	# raindance does not provide copy number or fusions data.
+	echo "removing unused files"
 	cd $MSK_RAINDANCE_DATA_HOME; rm data_CNA.txt; rm data_fusions.txt; rm data_SV.txt; rm mskimpact_data_cna_hg19.seg;
 	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest Raindance dataset"
 fi
 
 # fetch new/updated raindance samples using CVR Web service (must come after mercurial fetching). The -s flag skips segment data fetching
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/hemepact_fetcher.jar -d $MSK_HEMEPACT_DATA_HOME
-if [ $? -gt 0 ]
-then
-	echo "CVR heme fetch failed!"
-	echo "This will not affect importing of mskimpact"
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all;rm *.orig
-else
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest heme dataset"
-fi
+#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/hemepact_fetcher.jar -d $MSK_HEMEPACT_DATA_HOME
+#if [ $? -gt 0 ]
+#then
+	#echo "CVR heme fetch failed!"
+	#echo "This will not affect importing of mskimpact"
+	#cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all --no-backup;rm *.orig
+#else
+	#cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest heme dataset"
+#fi
 
 # fetch new/updated archer samples using CVR Web service (must come after mercurial fetching).
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/archer_fetcher.jar -d $MSK_ARCHER_DATA_HOME
-if [ $? -gt 0 ]
-then
-	echo "CVR Archer fetch failed!"
-	echo "This will not affect importing of mskimpact"
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all;rm *.orig
-else
-	cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest archer dataset"
-fi
+#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/archer_fetcher.jar -d $MSK_ARCHER_DATA_HOME
+#if [ $? -gt 0 ]
+#then
+	#echo "CVR Archer fetch failed!"
+	#echo "This will not affect importing of mskimpact"
+	#cd $MSK_IMPACT_DATA_HOME;$HG_BINARY revert --all --no-backup;rm *.orig
+#else
+	#cd $MSK_IMPACT_DATA_HOME;$HG_BINARY commit -m "Latest archer dataset"
+#fi
 
 # create case lists by cancer type
 rm $MSK_IMPACT_DATA_HOME/case_lists/*
@@ -121,25 +127,49 @@ $PYTHON_BINARY $PORTAL_HOME/scripts/create_case_lists_by_cancer_type.py --clinic
 cd $MSK_IMPACT_DATA_HOME;$HG_BINARY add;$HG_BINARY commit -m "Latest MSK-IMPACT Dataset: Case Lists"
 
 $PYTHON_BINARY $PORTAL_HOME/scripts/impact_timeline.py --hgrepo=$MSK_IMPACT_DATA_HOME
+sed -i '/^\s*$/d' $MSK_IMPACT_DATA_HOME/data_clinical_supp_date.txt
 cd $MSK_IMPACT_DATA_HOME; rm *.orig
+rm $MSK_IMPACT_DATA_HOME/case_lists/*.orig
 cd $MSK_IMPACT_DATA_HOME;$HG_BINARY add;$HG_BINARY commit -m "Latest MSK-IMPACT Dataset: Sample Date Clinical File"
 
-# import into portal database
-echo "importing cancer type updates into msk portal database..."
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --import-types-of-cancer
+
+# check database version before importing anything
+echo "Checking if database version is compatible"
+$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --check-db-version
+if [ $? -gt 0 ]
+then
+    echo "Database version expected by portal does not match version in database!"
+    DB_VERSION_FAIL=1
+    IMPORT_STATUS=1
+fi    
+
+if [ $DB_VERSION_FAIL -eq 0 ]
+then 
+    # import into portal database
+    echo "importing cancer type updates into msk portal database..."
+    $JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --import-types-of-cancer
+fi
+
 
 ## use flag to import under temporary id
 
 if [ $IMPORT_STATUS -eq 0 ]
 then
-	$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskimpact-portal --notification-file $mskimpact_notification_file
-	if [ $? -gt 0 ]
+	echo "Importing temporary mskimpact..."
+	$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskimpact-portal --notification-file "$mskimpact_notification_file" --temporary-id temporary_mskimpact
+	if [ -f "$tmp/num_studies_updated.txt" ]
+	then
+		num_studies_updated=`cat $tmp/num_studies_updated.txt`
+	else
+		num_studies_updated=0
+	fi
+	if [ "$num_studies_updated" -ne 1 ]
 	then
 		echo "Failed to import mskimpact!"
 		IMPORT_FAIL=1
 	else
-		num_studies_updated=`cat $tmp/num_studies_updated.txt`
 		# validate
+		echo "validating..."
 		$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --validate-temp-study --temp-study-id temporary_mskimpact --original-study-id mskimpact
 		if [ $? -gt 0 ]
 		then
@@ -154,24 +184,29 @@ then
 				echo "Failed to delete cancer study yesterday_mskimpact!"
 				DELETE_FAIL=1
 			else
-				$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --rename-cancer-study --new-study-id mskimpact --original-study-id yesterday_mskimpact
+				echo "Renaming mksimpact to yesterday_mskimpact"
+				$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --rename-cancer-study --new-study-id yesterday_mskimpact --original-study-id mskimpact
 				if [ $? -gt 0 ]
 				then
 					echo "Failed to rename old mskimpact study to yesterday!"
 					RENAME_BACKUP_FAIL=1
 				else
-					$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-groups "yesterday_mskimpact:KSBACKUP"
+					echo "Updating groups of yesterday_mskimpact to KSBACKUP"
+					$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-groups --cancer-study-ids yesterday_mskimpact --groups "KSBACKUP"
 					if [ $? -gt 0 ]
 					then
 						echo "Failed to change groups!"
 						GROUPS_FAIL=1
 					fi
+					echo "renaming temporary_mskimpact to mskimpact"
 					$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --rename-cancer-study --new-study-id mskimpact --original-study-id temporary_mskimpact
+					SUCCESS=1
 					if [ $? -gt 0 ]
 					then
 						echo "Failed to rename temp study to mskimpact!"
 						RENAME_FAIL=1
 					else
+						echo "Consuming samples from cvr"
 						$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -jar $PORTAL_HOME/lib/cvr_fetcher.jar -c $MSK_IMPACT_DATA_HOME/cvr_data.json
 					fi
 				fi
@@ -179,9 +214,15 @@ then
 		fi
 	fi
 
-	$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskraindance-portal --notification-file $mskraindance_notification_file
-	$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskheme-portal --notification-file $mskheme_notification_file
-
+	#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskraindance-portal --notification-file "$mskraindance_notification_file"
+	#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx64g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --update-study-data --portal mskheme-portal --notification-file "$mskheme_notification_file"
+else
+    if [ $DB_VERSION_FAIL -gt 0 ]
+    then
+        echo "Not importing - database version is not compatible"
+    else
+    	echo "Not importing - something went wrong with a fetch"
+    fi
 fi
 
 # redeploy war
@@ -201,12 +242,28 @@ cd $MSK_IMPACT_DATA_HOME;$HG_BINARY push
 
 ### FAILURE EMAIL ###
 
-EMAIL_BODY="The MSKIMPACT study failed import. The original study will remain on the portal."
-# send email if validation fails
+EMAIL_BODY="The MSKIMPACT database version is incompatible. Imports will be skipped until database is updated."
+# send email if db version isn't compatible
+if [ $DB_VERSION_FAIL -gt 0 ]
+then
+    echo -e "Sending email $EMAIL_BODY"
+    echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: DB version is incompatible" $email_list
+fi
+
+EMAIL_BODY="The MSKIMPACT study failed fetch. The original study will remain on the portal."
+# send email if fetch fails
 if [ $IMPORT_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Import" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Fetch Failure: Import" $email_list
+fi
+
+EMAIL_BODY="The MSKIMPACT study failed import. The original study will remain on the portal."
+# send email if import fails
+if [ $IMPORT_FAIL -gt 0 ]
+then
+	echo -e "Sending email $EMAIL_BODY"
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Import" $email_list
 fi
 
 EMAIL_BODY="The MSKIMPACT study failed to pass the validation step in import process. The original study will remain on the portal."
@@ -214,14 +271,14 @@ EMAIL_BODY="The MSKIMPACT study failed to pass the validation step in import pro
 if [ $VALIDATION_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Validation" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Validation" $email_list
 fi
 
 EMAIL_BODY="The yesterday_mskimpact study failed to delete. MSKIMPACT study did not finish updating."
 if [ $DELETE_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Deletion" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Deletion" $email_list
 fi
 
 
@@ -229,26 +286,26 @@ EMAIL_BODY="Failed to backup mskimpact to yesterday_mskimpact via renaming. MSKI
 if [ $RENAME_BACKUP_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Renaming backup" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Renaming backup" $email_list
 fi
 
 EMAIL_BODY="Failed to rename temp study temporary_mskimpact to mskimpact. MSKIMPACT study did not finish updating."
 if [ $RENAME_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: CRITIAL!! Renaming" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: CRITICAL!! Renaming" $email_list
 fi
 
 EMAIL_BODY="Failed to update groups for backup study."
 if [ $GROUPS_FAIL -gt 0 ]
 then
 	echo -e "Sending email $EMAIL_BODY"
-	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Groups update" cbioportal-cmo-importer@cbio.mskcc.org
+	echo -e "$EMAIL_BODY" | mail -s "MSKIMPACT Update Failure: Groups update" $email_list
 fi
 
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskimpact-portal --notification-file $mskimpact_notification_file
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskraindance-portal --notification-file $mskraindance_notification_file
-$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskheme-portal --notification-file $mskheme_notification_file
+$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskimpact-portal --notification-file "$mskimpact_notification_file"
+#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskraindance-portal --notification-file $mskraindance_notification_file
+#$JAVA_HOME/bin/java -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=27182 -Xmx16g -ea -Dspring.profiles.active=dbcp -Djava.io.tmpdir="$tmp" -cp $PORTAL_HOME/lib/msk-dmp-importer.jar org.mskcc.cbio.importer.Admin --send-update-notification --portal mskheme-portal --notification-file $mskheme_notification_file
 
 if [[ -d "$tmp" && "$tmp" != "/" ]]; then
 	rm -rf "$tmp"/*
