cc2bbf9b5c94ec66c59f40dc8a4f3fa86ace4a6f
Angelica Ochoa <aochoa4230@gmail.com>
2017-07-25 18:51:29 -0400

    Lymphoma Super Cohort
    The subset and merge of the lymphoma super cohort is now done after the final MSK affiliate subset/import (queens cancer center).
    The portal column name has been added to our configuration spreadsheet tabs, and another PR has been made in `knowledgesystems/pipelines` for sending notification file.
    
    Signed-off-by: Angelica Ochoa <aochoa4230@gmail.com>

--- import-scripts/automation-environment.sh
+++ import-scripts/automation-environment.sh
@@ -12,6 +12,8 @@ export MSK_LEHIGH_DATA_HOME=$PORTAL_DATA_HOME/msk-impact/msk_lehighvalley
 export MSK_QUEENS_DATA_HOME=$PORTAL_DATA_HOME/msk-impact/msk_queenscancercenter
 export IMPACT_DATA_HOME=$PORTAL_DATA_HOME/impact
 export FOUNDATION_DATA_HOME=$PORTAL_DATA_HOME/foundation
+export FMI_BATLEVI_DATA_HOME=$FOUNDATION_DATA_HOME/mixed/lymphoma/mskcc/foundation/lymph_landscape_fmi_201611
+export LYMPHOMA_SUPER_COHORT_DATA_HOME=$PORTAL_DATA_HOME/studies/lymphoma_super_cohort_fmi_msk
 # CMO_DATA_HOME looks unused
 #export CMO_DATA_HOME=/data/cbio-portal-data/bic-mskcc
 export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk.x86_64
