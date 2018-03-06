59ecc5baffdbea145abf06f9b80500edd9a685ae
Robert Sheridan <sheridan@cbio.mskcc.org>
2018-03-06 11:40:47 -0500

    Altered crontab to match production, and logging timestamps - production updated to include mailed warnings - dropped obsolete call to scan-for-stalled-import - dropped triage "one-time" placeholder line Logging of timestamps added to import users scripts - redirect with append instead of overwrite

--- import-scripts/import-portal-users.sh
+++ import-scripts/import-portal-users.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-$PYTHON_BINARY $PORTAL_HOME/scripts/importUsers.py --port 3306 --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi.gdac --send-email-confirm true > $PORTAL_HOME/logs/import-user-dashi-gdac.log
+USERSDASHILOGFILENAME="$PORTAL_HOME/logs/import-user-dashi-gdac.log"
+USERSDASHI2LOGFILENAME="$PORTAL_HOME/logs/import-user-dashi2.log"
+CANCERSTUDIESLOGFILENAME="$PORTAL_HOME/logs/update-studies-dashi-gdac.log"
 
-$PYTHON_BINARY $PORTAL_HOME/scripts/importUsers.py --port 3306 --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi2 --send-email-confirm true > $PORTAL_HOME/logs/import-user-dashi2.log
+echo "### Starting import" >> "$USERSDASHILOGFILENAME"
+date >> "$USERSDASHILOGFILENAME"
+$PYTHON_BINARY $PORTAL_HOME/scripts/importUsers.py --port 3306 --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi.gdac --send-email-confirm true >> "$USERSDASHILOGFILENAME" 2>&1
 
-$PYTHON_BINARY $PORTAL_HOME/scripts/updateCancerStudies.py --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi.gdac --send-email-confirm true > $PORTAL_HOME/logs/update-studies-dashi-gdac.log
+echo "### Starting import" >> "$USERSDASHI2LOGFILENAME"
+date >> "$USERSDASHI2LOGFILENAME"
+$PYTHON_BINARY $PORTAL_HOME/scripts/importUsers.py --port 3306 --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi2 --send-email-confirm true >> "$USERSDASHI2LOGFILENAME" 2>&1
+
+echo "### Starting import" >> "$CANCERSTUDIESLOGFILENAME"
+date >> "$CANCERSTUDIESLOGFILENAME"
+$PYTHON_BINARY $PORTAL_HOME/scripts/updateCancerStudies.py --secrets-file $PORTAL_DATA_HOME/portal-configuration/google-docs/client_secrets.json --creds-file $PORTAL_DATA_HOME/portal-configuration/google-docs/creds.dat --properties-file $PORTAL_HOME/cbio-portal-data/portal-configuration/properties/import-users/portal.properties.dashi.gdac --send-email-confirm true >> "$CANCERSTUDIESLOGFILENAME" 2>&1
