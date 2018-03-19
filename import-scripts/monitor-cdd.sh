7da4739a552b0e5f83bf57d584ee60e4a4947387
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2018-03-19 12:24:34 -0400

    monitors CDD for stale cache (or any non-200 status code)

---
+++ import-scripts/monitor-cdd.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+URL="http://oncotree.mskcc.org/cdd/api/"
+SERVICE_UNAVAILABLE=503
+OK=200
+
+http_code=`curl -o /dev/null --silent --write-out '%{http_code}\n' $URL`
+if [ "$http_code" -ne $OK ]
+then
+    (>&2 echo "GET '$URL' returned '$http_code' status code, expected '$OK'")
+    if [ "$http_code" -eq $SERVICE_UNAVAILABLE ]
+    then
+        (>&2 echo "The CDD cache is empty, attempts to refresh the cache from Google must have failed")
+    fi
+fi
