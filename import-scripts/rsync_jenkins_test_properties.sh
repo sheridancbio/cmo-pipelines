a17b60c38254ac5e0db1aabd9228073656d86be8
Manda Wilson <1458628+mandawilson@users.noreply.github.com>
2018-08-21 11:19:33 -0400

    rsyncing jenkins scripts directory to jenkins machine

--- import-scripts/rsync_jenkins_test_properties.sh
+++ import-scripts/rsync_jenkins_test_properties.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 PATH_TO_AUTOMATION_SCRIPT=/data/portal-cron/scripts/automation-environment.sh
-JENKINS_HOSTNAME=jenkins_hostname_prod
-JENKINS_HOME_DIRECTORY=/var/lib/jenkins
-JENKINS_PROPERTIES_DIRECTORY=$JENKINS_HOME_DIRECTORY/pipelines-configuration/properties
-JENKINS_PIPELINES_CREDENTIALS=$JENKINS_HOME_DIRECTORY/pipelines-credentials
-JENKINS_GIT_CREDENTIALS=$JENKINS_HOME_DIRECTORY/git-credentials
+# jenkins server paths
+JENKINS_SRV_HOSTNAME=jenkins_hostname_prod
+JENKINS_SRV_HOME_DIRECTORY=/var/lib/jenkins
+JENKINS_SRV_PROPERTIES_DIRECTORY=$JENKINS_SRV_HOME_DIRECTORY/pipelines-configuration/properties
+JENKINS_SRV_SCRIPTS_DIRECTORY=$JENKINS_SRV_HOME_DIRECTORY/pipelines-configuration/jenkins
+JENKINS_SRV_PIPELINES_CREDENTIALS=$JENKINS_SRV_HOME_DIRECTORY/pipelines-credentials
+JENKINS_SRV_GIT_CREDENTIALS=$JENKINS_SRV_HOME_DIRECTORY/git-credentials
 # Function for alerting slack channel that something failed
 function sendFailureMessageMskPipelineLogsSlack {
     curl -X POST --data-urlencode "payload={\"channel\": \"#msk-pipeline-logs\", \"username\": \"cbioportal_importer\", \"text\": \"$1\", \"icon_emoji\": \":boom:\"}" https://hooks.slack.com/services/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
@@ -17,11 +19,13 @@ if ! [ -f $PATH_TO_AUTOMATION_SCRIPT ] ; then
 fi
 
 . $PATH_TO_AUTOMATION_SCRIPT
-PROPERTIES_DIRECTORY=$PIPELINES_CONFIG_HOME/properties/
-PIPELINES_CREDENTIALS=$PORTAL_HOME/pipelines-credentials/
-GIT_CREDENTIALS=$PORTAL_CONFIG_HOME/git/git-credentials
+# local jenkins staging paths
+LOCAL_PROPERTIES_DIRECTORY=$PIPELINES_CONFIG_HOME/properties/
+LOCAL_JENKINS_DIRECTORY=$PIPELINES_CONFIG_HOME/jenkins/
+LOCAL_PIPELINES_CREDENTIALS=$PORTAL_HOME/pipelines-credentials/
+LOCAL_GIT_CREDENTIALS=$PORTAL_CONFIG_HOME/git/git-credentials
 
-cd $PROPERTIES_DIRECTORY
+cd $LOCAL_PROPERTIES_DIRECTORY
 git pull
 if [ $? -ne 0 ] ; then
     FAILURE_MESSAGE="Something went wrong when pulling pipelines-configuration git repo"
@@ -29,32 +33,39 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-rsync -a $PROPERTIES_DIRECTORY $JENKINS_HOSTNAME:$JENKINS_PROPERTIES_DIRECTORY
+rsync -a $LOCAL_PROPERTIES_DIRECTORY $JENKINS_SRV_HOSTNAME:$JENKINS_SRV_PROPERTIES_DIRECTORY
 if [ $? -ne 0 ] ; then
     FAILURE_MESSAGE="Something went wrong when rsync-ing properties to jenkins machine"
     sendFailureMessageMskPipelineLogsSlack "$FAILURE_MESSAGE"
     exit 1
 fi
 
-rsync -a $PIPELINES_CREDENTIALS $JENKINS_HOSTNAME:$JENKINS_PIPELINES_CREDENTIALS
+rsync -a $LOCAL_JENKINS_DIRECTORY $JENKINS_SRV_HOSTNAME:$JENKINS_SRV_SCRIPTS_DIRECTORY
+if [ $? -ne 0 ] ; then
+    FAILURE_MESSAGE="Something went wrong when rsync-ing jenkins scripts to jenkins machine"
+    sendFailureMessageMskPipelineLogsSlack "$FAILURE_MESSAGE"
+    exit 1
+fi
+
+rsync -a $LOCAL_PIPELINES_CREDENTIALS $JENKINS_SRV_HOSTNAME:$JENKINS_SRV_PIPELINES_CREDENTIALS
 if [ $? -ne 0 ] ; then
     FAILURE_MESSAGE="Something went wrong when rsync-ing pipelines-credentials to jenkins machine"
     sendFailureMessageMskPipelineLogsSlack "$FAILURE_MESSAGE"
     exit 1
 fi
 
-rsync -a $GIT_CREDENTIALS $JENKINS_HOSTNAME:$JENKINS_HOME_DIRECTORY
+rsync -a $LOCAL_GIT_CREDENTIALS $JENKINS_SRV_HOSTNAME:$JENKINS_SRV_HOME_DIRECTORY
 if [ $? -ne 0 ] ; then
     FAILURE_MESSAGE="Something went wrong when rsync-ing git-credentials to jenkins machine"
     sendFailureMessageMskPipelineLogsSlack "$FAILURE_MESSAGE"
     exit 1
 fi
 
-ssh $JENKINS_HOSTNAME /bin/bash << EOF
-	chmod -R 700 $JENKINS_PROPERTIES_DIRECTORY $JENKINS_PIPELINES_CREDENTIALS &&
-	find $JENKINS_PROPERTIES_DIRECTORY -type f -exec chmod 600 {} \; &&
-	find $JENKINS_PIPELINES_CREDENTIALS -type f -exec chmod 600 {} \; &&
-	chmod 600 $JENKINS_GIT_CREDENTIALS;
+ssh $JENKINS_SRV_HOSTNAME /bin/bash << EOF
+    chmod -R 700 $JENKINS_SRV_PROPERTIES_DIRECTORY $JENKINS_SRV_PIPELINES_CREDENTIALS &&
+    find $JENKINS_SRV_PROPERTIES_DIRECTORY -type f -exec chmod 600 {} \; &&
+    find $JENKINS_SRV_PIPELINES_CREDENTIALS -type f -exec chmod 600 {} \; &&
+    chmod 600 $JENKINS_SRV_GIT_CREDENTIALS;
 EOF
 
 if [ $? -ne 0 ] ; then
