9461d3e7511e7badad432632602da0a3b5fdff9f
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2019-11-05 10:49:22 -0500

    OncoKB annotator script bugs
    - add time stamps to oncokb annotator bash script
    - Run OncoKB annotator as separate job from DMP fetcher/importer wrapper
    - Use python2 instead of python3 for oncokb annotator scripts
    - Update crontab
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- import-scripts/oncokb-annotator.sh
+++ import-scripts/oncokb-annotator.sh
@@ -5,7 +5,7 @@ source $PORTAL_HOME/scripts/dmp-import-vars-functions.sh
 ONCOKB_ANNOTATION_SUCCESS=1
 
 # Make sure necessary environment variables are set before running
-if [ -z $PYTHON3_BINARY ] | [ -z $HG_BINARY ] | [ -z $ONCOKB_ANNOTATOR_HOME ] | [ -z $MSK_SOLID_HEME_DATA_HOME ] | [ -z $ONCOKB_URL ] | [ -z $CANCER_HOTSPOTS_URL ] ; then
+if [ -z $PYTHON_BINARY ] | [ -z $HG_BINARY ] | [ -z $ONCOKB_ANNOTATOR_HOME ] | [ -z $MSK_SOLID_HEME_DATA_HOME ] | [ -z $ONCOKB_URL ] | [ -z $CANCER_HOTSPOTS_URL ] ; then
     message="could not run oncokb annotation script: automation-environment.sh script must be run in order to set needed environment variables."
     echo $message
     echo -e "$message" |  mail -s "oncokb-annotator failed to run." $PIPELINES_EMAIL_LIST
@@ -14,8 +14,10 @@ if [ -z $PYTHON3_BINARY ] | [ -z $HG_BINARY ] | [ -z $ONCOKB_ANNOTATOR_HOME ] | 
 fi
 
 # Annotating MAF
+echo $(date)
+echo "Beginning MAF annotation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/MafAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_mutations_extended.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL -v $CANCER_HOTSPOTS_URL
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/MafAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_mutations_extended.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL -v $CANCER_HOTSPOTS_URL
     if [ $? -ne 0 ] ; then
         echo "Failed to annotate MAF, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -23,8 +25,10 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Annotating Fusions
+echo $(date)
+echo "Beginning fusions annotation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/FusionAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_fusions.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/FusionAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_fusions.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL
     if [ $? -ne 0 ] ; then
         echo "Failed to annotate fusion file, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -32,8 +36,10 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Annotating CNA
+echo $(date)
+echo "Beginning CNA annotation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/CnaAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_CNA.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/CnaAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_CNA.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt -c $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -u $ONCOKB_URL
     if [ $? -ne 0 ] ; then
         echo "Failed to annotate CNA file, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -41,8 +47,10 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Annotating Clinical
+echo $(date)
+echo "Beginning clinical annotation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/ClinicalDataAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.txt -a $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/ClinicalDataAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.txt -a $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt
     if [ $? -ne 0 ] ; then
         echo "Failed to annotate clinical file, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -50,8 +58,10 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Generating Clinical PDF
+echo $(date)
+echo "Beginning clinical PDF generation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/OncoKBPlots.py -i $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.pdf -n 100
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/OncoKBPlots.py -i $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample.oncokb.pdf -n 100
     if [ $? -ne 0 ] ; then
         echo "Failed to generate clinical pdf, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -59,11 +69,13 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Annotating Somatic Clinical
+echo $(date)
+echo "Beginning somatic clinical annotation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
     # Generate somatic-only MAF by excluding lines including 'GERMLINE'
     awk -F'\t' '$26 != "GERMLINE"' $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended.oncokb.txt >$MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended_somatic.oncokb.txt
 
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/ClinicalDataAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.txt -a $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended_somatic.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/ClinicalDataAnnotator.py -i $MSK_SOLID_HEME_DATA_HOME/data_clinical_sample.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.txt -a $MSK_SOLID_HEME_DATA_HOME/oncokb/data_mutations_extended_somatic.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_fusions.oncokb.txt,$MSK_SOLID_HEME_DATA_HOME/oncokb/data_CNA.oncokb.txt
     if [ $? -ne 0 ] ; then
         echo "Failed to annotate somatic clinical file, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -71,8 +83,10 @@ if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
 fi
 
 # Generating Somatic Clinical PDF
+echo $(date)
+echo "Beginning somatic clinical PDF generation..."
 if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-    $PYTHON3_BINARY $ONCOKB_ANNOTATOR_HOME/OncoKBPlots.py -i $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.pdf -n 100
+    $PYTHON_BINARY $ONCOKB_ANNOTATOR_HOME/OncoKBPlots.py -i $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.txt -o $MSK_SOLID_HEME_DATA_HOME/oncokb/data_clinical_sample_somatic.oncokb.pdf -n 100
     if [ $? -ne 0 ] ; then
         echo "Failed to generate somatic clinical pdf, exiting..."
         ONCOKB_ANNOTATION_SUCCESS=0 
@@ -90,3 +104,6 @@ else
     echo "committing OncoKB Annotation for MSKSOLIDHEME"
     cd $MSK_SOLID_HEME_DATA_HOME ; $HG_BINARY commit -m "Latest MSKSOLIDHEME OncoKB Annotations"
 fi
+
+echo $(date)
+echo "OncoKB annotations complete"
