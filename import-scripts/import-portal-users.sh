3bb95a8b046e1648ad03318d7f99c04e7a39f9a1
Avery Wang <wanga5@mskcc.org>
2021-02-16 14:54:43 -0500

    remove user imports to cgds_gdac on pipelines because of transition to keycloak

--- import-scripts/import-portal-users.sh
+++ import-scripts/import-portal-users.sh
@@ -1,25 +1,13 @@
 #!/usr/bin/env bash
 
 MY_FLOCK_FILEPATH="/data/portal-cron/cron-lock/import-portal-users.lock"
-PURGE_DEACTIVATED_LDAP_USERS_FLOCK_FILEPATH="/data/portal-cron/cron-lock/purge_deactivated_ldap_users.lock"
 (
     # check lock so that script executions do not overlap
     if ! flock --nonblock --exclusive $my_flock_fd ; then
         exit 0
     fi
 
-    # abort if purge_deactivated_ldap_users.sh is currently executing
-    if ! flock --nonblock --exclusive $purge_deactivated_ldap_users_flock_fd ; then
-        exit 0
-    fi
-
     USERSGENIELOGFILENAME="$PORTAL_HOME/logs/import-user-dashi-genie.log"
-    USERSDASHILOGFILENAME="$PORTAL_HOME/logs/import-user-dashi-gdac.log"
-
-    echo "### Starting import" >> "$USERSDASHILOGFILENAME"
-    date >> "$USERSDASHILOGFILENAME"
-    $PYTHON_BINARY $PORTAL_HOME/scripts/importUsers.py --port 3306 --secrets-file $PIPELINES_CONFIG_HOME/google-docs/client_secrets.json --creds-file $PIPELINES_CONFIG_HOME/google-docs/creds.dat --properties-file $PIPELINES_CONFIG_HOME/properties/import-users/portal.properties.dashi.gdac --send-email-confirm true >> "$USERSDASHILOGFILENAME" 2>&1
-    CGDS_GDAC_IMPORT_STATUS=$?
 
     echo "### Starting import" >> "$USERSGENIELOGFILENAME"
     date >> "$USERSGENIELOGFILENAME"
@@ -33,9 +21,6 @@ PURGE_DEACTIVATED_LDAP_USERS_FLOCK_FILEPATH="/data/portal-cron/cron-lock/purge_d
     
     PIPELINES_EMAIL_LIST="cbioportal-pipelines@cbio.mskcc.org"
     FAILED_DATABASES=""
-    if [[ $CGDS_GDAC_IMPORT_STATUS -ne 0 ]] ; then
-        FAILED_DATABASES="$FAILED_DATABASES cgds_gdac"
-    fi
     if [[ $CGDS_GENIE_IMPORT_STATUS -ne 0 ]] ; then
         FAILED_DATABASES="$FAILED_DATABASES cgds_genie"
     fi
@@ -50,6 +35,6 @@ PURGE_DEACTIVATED_LDAP_USERS_FLOCK_FILEPATH="/data/portal-cron/cron-lock/purge_d
         fi
     fi
 
-) {my_flock_fd}>$MY_FLOCK_FILEPATH {purge_deactivated_ldap_users_flock_fd}>$PURGE_DEACTIVATED_LDAP_USERS_FLOCK_FILEPATH
+) {my_flock_fd}>$MY_FLOCK_FILEPATH
 
 exit 0
