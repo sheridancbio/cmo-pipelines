8e7dd12cf048c751cc702e970f8539a22d631af2
averyniceday <wanga5@mskcc.org>
2021-09-08 12:51:32 -0700

    Separate EKS vs Pipelines Scripts (#859)
    * split scripts into eks vs pipelines deployed
    * Update monitor-crontab-version.sh
    
    Co-authored-by: Avery Wang <averyjwang@gmail.com>
    Co-authored-by: Robert Sheridan <7747489+sheridancbio@users.noreply.github.com>

--- import-scripts/monitor-crontab-version.sh
+++ import-scripts/monitor-crontab-version.sh
@@ -1,7 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# function for sending notification emails
-send_email_notification() {
+if [ -z "$PORTAL_HOME" ] | [ -z "$PIPELINES_CONFIG_HOME" ] ; then
+    echo "monitor-crontab-version.sh could not be run: missing environment variables must be set using automation-environment.sh" >&2
+    exit 1
+fi
+
+GITHUB_PIPELINES_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/pipelines/mycrontab"
+GITHUB_PIPELINES_EKS_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/pipelines_eks/mycrontab"
+GITHUB_AUTHORIZATION=$(cat $PIPELINES_CONFIG_HOME/git/git-credentials | sed 's/https:\/\///; s/@github\.com//')
+CURRENT_CRONTAB_FILE="$PORTAL_HOME/tmp/current_crontab"
+GITHUB_CRONTAB_FILE="$PORTAL_HOME/tmp/git_crontab"
+REQUIRED_USERNAME="cbioportal_importer"
+PIPELINES_HOST_SUFFIX="pipelines.cbioportal.mskcc.org"
+PIPELINES_EKS_HOST_SUFFIX=".mskcc.root.mskcc.org"
+
+function send_email_notification() {
     diff_report=$1
     ### FAILURE EMAIL ###
     EMAIL_BODY="The current (pipelines) crontab is out of sync with the crontab in github.\n\n $diff_report"
@@ -9,26 +22,27 @@ send_email_notification() {
     echo -e "$EMAIL_BODY" | mail -s "Alert: Crontab out of sync on $HOSTNAME" cbioportal-pipelines@cbio.mskcc.org
 }
 
-if [ -z $PORTAL_HOME ] | [ -z $PIPELINES_CONFIG_HOME ] ; then
-    echo "monitor-crontab-version.sh could not be run: missing environment variables must be set using automation-environment.sh"
+if [ "$USER" != "$REQUIRED_USERNAME" ] ; then
+    echo "monitor-crontab-version.sh must be run as $REQUIRED_USERNAME" >&2
     exit 1
 fi
 
-GITHUB_CRONTAB_URL="https://api.github.com/repos/knowledgesystems/cmo-pipelines/contents/import-scripts/mycrontab"
-GITHUB_AUTHORIZATION=$(cat $PIPELINES_CONFIG_HOME/git/git-credentials | sed 's/https:\/\///; s/@github\.com//')
-CURRENT_CRONTAB_FILE="$PORTAL_HOME/tmp/current_crontab"
-GITHUB_CRONTAB_FILE="$PORTAL_HOME/tmp/git_crontab"
-REQUIRED_USERNAME="cbioportal_importer"
-REQUIRED_HOSTNAME="pipelines.cbioportal.mskcc.org"
+# returns status zero (success) if string ends with suffix
+function has_suffix() {
+    string=$1
+    suffix=$2
+    [[ "$string" =~ .*"$suffix"$ ]]
+}
 
-if [ $USER != $REQUIRED_USERNAME ] ; then
-    echo "monitor-crontab-version.sh must be run as $REQUIRED_USERNAME"
-    exit 1
-fi
-
-if [ $HOSTNAME != $REQUIRED_HOSTNAME ] ; then
-    echo "monitor-crontab-version.sh  must be run on $HOSTNAME"
-    exit 1
+if has_suffix "$HOSTNAME" "$PIPELINES_HOST_SUFFIX" ; then
+    GITHUB_CRONTAB_URL="$GITHUB_PIPELINES_CRONTAB_URL"
+else
+    if has_suffix "$HOSTNAME" "$PIPELINES_EKS_HOST_SUFFIX" ; then
+        GITHUB_CRONTAB_URL=$GITHUB_PIPELINES_EKS_CRONTAB_URL
+    else
+        echo "error : monitor-crontab-version.sh must be run on either $PIPELINES_HOST_SUFFIX or an eks node with hostname like *$PIPELINES_EKS_HOST_SUFFIX" >&2
+        exit 1
+    fi
 fi
 
 rm -f $PORTAL_HOME/tmp/git_crontab
@@ -36,7 +50,7 @@ rm -f $PORTAL_HOME/tmp/current_crontab
 
 curl -f -u "$GITHUB_AUTHORIZATION" -H "Accept: application/vnd.github.v4.raw" -L -o $GITHUB_CRONTAB_FILE $GITHUB_CRONTAB_URL
 if [ $? -gt 0 ] ; then
-    echo "There was an error retrieving crontab from github -- curl returned a non-zero exit status"
+    echo "There was an error retrieving crontab from github -- curl returned a non-zero exit status" >&2
     exit 1
 fi
 crontab -l > $CURRENT_CRONTAB_FILE
---
+++ import-scripts/pipelines_eks/automation-environment.sh
@@ -0,0 +1,133 @@
+#!/bin/bash
+
+#######################
+# general paths/options for system executables
+#######################
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-1.8.0.282.b08-1.amzn2.0.1.x86_64
+export JAVA_PROXY_ARGS="-Dhttp.proxyHost=jxi2.mskcc.org -Dhttp.proxyPort=8080 -Dhttp.nonProxyHosts=draco.mskcc.org|pidvudb1.mskcc.org|phcrdbd2.mskcc.org|dashi-dev.cbio.mskcc.org|pipelines.cbioportal.mskcc.org|localhost"
+export JAVA_BINARY=$JAVA_HOME/bin/java
+export PYTHON_BINARY=/usr/bin/python
+export PYTHON3_BINARY=/usr/bin/python3
+export MAVEN_BINARY=/opt/apache-maven-3.8.1/bin/mvn
+export HG_BINARY=/usr/bin/hg
+export GIT_BINARY=/usr/bin/git
+export PATH=/opt:/usr/bin:/data/tools/python2.7/bin:/data/tools/python3.4.10/bin:$PATH
+
+#######################
+# environment variables for top-level data repositories / code bases
+#######################
+export PORTAL_HOME=/data/portal-cron
+export PORTAL_DATA_HOME=$PORTAL_HOME/cbio-portal-data
+
+#######################
+# environment variables for our git code bases
+#######################
+export PORTAL_GIT_HOME=$PORTAL_HOME/git-repos
+export CMO_PIPELINES_HOME=$PORTAL_GIT_HOME/cmo-pipelines
+export PIPELINES_HOME=$PORTAL_GIT_HOME/pipelines
+export CBIOPORTAL_HOME=$PORTAL_GIT_HOME/cbioportal
+export GENOME_NEXUS_ANNOTATOR_HOME=$PORTAL_GIT_HOME/genome-nexus-annotation-pipeline
+export ANNOTATOR_JAR=$GENOME_NEXUS_ANNOTATOR_HOME/annotationPipeline/target/annotationPipeline*.jar
+export ONCO_HOME=$PORTAL_GIT_HOME/oncotree
+export ONCOKB_ANNOTATOR_HOME=$PORTAL_GIT_HOME/oncokb-annotator
+export CDD_HOME=$PORTAL_GIT_HOME/clinical-data-dictionary
+export DDP_CREDENTIALS_FILE=$PORTAL_HOME/pipelines-credentials/application-secure.properties
+export AWS_SSL_TRUSTSTORE=$PORTAL_HOME/pipelines-credentials/AwsSsl.truststore
+export AWS_SSL_TRUSTSTORE_PASSWORD_FILE=$PORTAL_HOME/pipelines-credentials/AwsSsl.truststore.password
+export SLACK_URL_FILE=$PORTAL_HOME/pipelines-credentials/slack.url
+
+#######################
+# SSL args (for AWS + redcap)
+#######################
+export JAVA_SSL_ARGS="-Djavax.net.ssl.trustStore=$AWS_SSL_TRUSTSTORE -Djavax.net.ssl.trustStorePassword=`cat $AWS_SSL_TRUSTSTORE_PASSWORD_FILE`"
+
+#######################
+# environment variables for configuration / properties files
+#######################
+export PORTAL_CONFIG_HOME=$PORTAL_DATA_HOME/portal-configuration
+export PIPELINES_CONFIG_HOME=$PORTAL_GIT_HOME/pipelines-configuration
+
+#######################
+# environment variables for top level data repositories
+#######################
+export BIC_DATA_HOME=$PORTAL_DATA_HOME/bic-mskcc
+export PDX_DATA_HOME=$PORTAL_DATA_HOME/crdb_pdx
+export PRIVATE_DATA_HOME=$PORTAL_DATA_HOME/private
+export DMP_DATA_HOME=$PORTAL_DATA_HOME/dmp
+export FOUNDATION_DATA_HOME=$PORTAL_DATA_HOME/foundation
+export IMPACT_DATA_HOME=$PORTAL_DATA_HOME/impact
+export DATAHUB_DATA_HOME=$PORTAL_DATA_HOME/datahub/public
+export MSK_MIND_DATA_HOME=$PORTAL_DATA_HOME/msk-mind
+export MSK_SHAHLAB_DATA_HOME=$PORTAL_DATA_HOME/datahub_shahlab
+
+#######################
+# environment variables used across import scripts
+#######################
+#export INHIBIT_RECACHING_FROM_TOPBRAID=true
+export CASE_LIST_CONFIG_FILE=$PIPELINES_CONFIG_HOME/resources/case_list_config.tsv
+
+#######################
+# environment variables used in the fetch-and-import-dmp-impact-data script
+#######################
+# trigger files to communicate between fetch-dmp-data and import-dmp-data
+export MSK_DMP_TMPDIR=$PORTAL_HOME/tmp/import-cron-dmp-msk
+export MSK_IMPACT_CONSUME_TRIGGER=$MSK_DMP_TMPDIR/mskimpact_consume_trigger.txt
+export MSK_HEMEPACT_CONSUME_TRIGGER=$MSK_DMP_TMPDIR/mskimpact_heme_consume_trigger.txt
+export MSK_ARCHER_CONSUME_TRIGGER=$MSK_DMP_TMPDIR/mskarcher_consume_trigger.txt
+export MSK_ACCESS_CONSUME_TRIGGER=$MSK_DMP_TMPDIR/mskaccess_consume_trigger.txt
+export MSK_ARCHER_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/mskarcher_import_trigger.txt
+export MSK_SOLID_HEME_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/msk_solid_heme_import_trigger.txt
+export MSK_KINGS_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/kingscounty_import_trigger.txt
+export MSK_LEHIGH_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/lehighvalley_import_trigger.txt
+export MSK_QUEENS_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/queenscancercenter_import_trigger.txt
+export MSK_MCI_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/miamicancerinstitute_import_trigger.txt
+export MSK_HARTFORD_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/hartfordhealthcare_import_trigger.txt
+export MSK_RALPHLAUREN_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/ralphlauren_import_trigger.txt
+export MSK_RIKENGENESISJAPAN_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/msk_rikengenesisjapan_import_trigger.txt
+export MSK_SCLC_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/sclc_mskimpact_import_trigger.txt
+export MSKIMPACT_PED_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/mskimpact_ped_import_trigger.txt
+export LYMPHOMA_SUPER_COHORT_IMPORT_TRIGGER=$MSK_DMP_TMPDIR/lymphoma_super_cohort_fmi_msk_import_trigger.txt
+# data directories
+export MSK_IMPACT_DATA_HOME=$DMP_DATA_HOME/mskimpact
+export MSK_RAINDANCE_DATA_HOME=$DMP_DATA_HOME/mskraindance
+export MSK_HEMEPACT_DATA_HOME=$DMP_DATA_HOME/mskimpact_heme
+export MSK_ARCHER_DATA_HOME=$DMP_DATA_HOME/mskarcher
+export MSK_ARCHER_UNFILTERED_DATA_HOME=$DMP_DATA_HOME/mskarcher_unfiltered
+export MSK_ACCESS_DATA_HOME=$DMP_DATA_HOME/mskaccess
+export MSK_MIXEDPACT_DATA_HOME=$DMP_DATA_HOME/mixedpact
+export MSK_SOLID_HEME_DATA_HOME=$DMP_DATA_HOME/msk_solid_heme
+export MSK_KINGS_DATA_HOME=$DMP_DATA_HOME/msk_kingscounty
+export MSK_LEHIGH_DATA_HOME=$DMP_DATA_HOME/msk_lehighvalley
+export MSK_QUEENS_DATA_HOME=$DMP_DATA_HOME/msk_queenscancercenter
+export MSK_MCI_DATA_HOME=$DMP_DATA_HOME/msk_miamicancerinstitute
+export MSK_HARTFORD_DATA_HOME=$DMP_DATA_HOME/msk_hartfordhealthcare
+export MSK_RALPHLAUREN_DATA_HOME=$DMP_DATA_HOME/msk_ralphlauren
+export MSK_RIKENGENESISJAPAN_DATA_HOME=$DMP_DATA_HOME/msk_rikengenesisjapan
+export MSK_SCLC_DATA_HOME=$DMP_DATA_HOME/sclc_mskimpact_2017
+export MSKIMPACT_PED_DATA_HOME=$DMP_DATA_HOME/mskimpact_ped
+export LYMPHOMA_SUPER_COHORT_DATA_HOME=$DMP_DATA_HOME/lymphoma_super_cohort_fmi_msk
+export MSK_EXTRACT_COHORT_DATA_HOME=$MSK_MIND_DATA_HOME/datahub/msk_extract_cohort2_2019
+export MSK_SPECTRUM_COHORT_DATA_HOME=$MSK_SHAHLAB_DATA_HOME/msk_spectrum
+# read-only data directories
+export FMI_BATLEVI_DATA_HOME=$FOUNDATION_DATA_HOME/mixed/lymphoma/mskcc/foundation/lymph_landscape_fmi_201611
+
+#######################
+# environment variables used in the backup-redcap-data.sh script
+#######################
+export REDCAP_BACKUP_DATA_HOME=$PORTAL_DATA_HOME/redcap-snapshot
+export MSKIMPACT_REDCAP_BACKUP=$REDCAP_BACKUP_DATA_HOME/mskimpact
+export HEMEPACT_REDCAP_BACKUP=$REDCAP_BACKUP_DATA_HOME/mskimpact_heme
+export ARCHER_REDCAP_BACKUP=$REDCAP_BACKUP_DATA_HOME/mskarcher
+export ACCESS_REDCAP_BACKUP=$REDCAP_BACKUP_DATA_HOME/mskaccess
+
+#######################
+# environment variables used in the import-pdx-data script
+#######################
+export CRDB_FETCHER_PDX_HOME=$PDX_DATA_HOME/crdb_pdx_raw_data
+
+#######################
+# environment variables used for oncokb annotator script
+#######################
+export ONCOKB_URL="http://www.oncokb.org"
+export ONCOKB_TOKEN_FILE=$PORTAL_HOME/pipelines-credentials/oncokb.token
+export CANCER_HOTSPOTS_URL="http://www.cancerhotspots.org"
