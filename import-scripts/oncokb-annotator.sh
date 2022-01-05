53dc40c2b2b053ac13b6add00f468a57f74c672f
Robert Sheridan <sheridan@ip-10-1-141-114.cbioportal.aws.mskcc.org>
2022-01-05 17:21:56 -0500

    oncokb-annotator output goes to github

--- import-scripts/oncokb-annotator.sh
+++ import-scripts/oncokb-annotator.sh
@@ -11,7 +11,7 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
 
     source $PORTAL_HOME/scripts/set-data-source-environment-vars.sh
 
-    function send_failure_messages () {
+    function send_failure_messages() {
         email_message=$1
         email_subject=$2
         slack_message=$3
@@ -20,39 +20,14 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
         sendPreImportFailureMessageMskPipelineLogsSlack "$slack_message"
     }
 
-    function git_clone_is_up_to_date () {
-        clone_dir=$1
-        git -C "$clone_dir" fetch
-        diff_output=$(git -C "$clone_dir" diff origin/master | head -n 1)
+    function oncokb_annotator_code_is_up_to_date() {
+        code_dir="$ONCOKB_ANNOTATOR_HOME"
+        git -C "$code_dir" fetch
+        diff_output=$(git -C "$code_dir" diff origin/master | head -n 1)
         if [ -z "$diff_output" ] ; then
             return 0
         fi
         return 1
-    }
-
-    function gzip_oncokb_annotated_files () {
-        if ! gzip -7 --force $ONCOKB_OUT_DIR/data*.txt ; then
-            message="Error during attempt to gzip $ONCOKB_OUT_DIR data files"
-            send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
-            exit 1
-        fi
-    }
-
-    function rsync_oncokb_annotated_files () {
-        CREATE_INDEX_SCRIPT="$PORTAL_HOME/scripts/create_web_directory_index_file.sh"
-        TMP_DIR="/data/portal-cron/tmp/rsync_oncokb"
-        INDEX_FILE="$TMP_DIR/index.html"
-        TITLE_STRING="oncokb-annotation-msk-impact"
-        BASE_URL="http://download.cbioportal.org/oncokb-annotated-msk-impact"
-        REMOTE_HOST="ramen.cbio.mskcc.org"
-        REMOTE_DIR="/data/public/oncokb-annotated-msk-impact"
-        RSYNC_TARGET="$REMOTE_HOST:$REMOTE_DIR"
-        RSYNC_IDENTITY_FILE="/home/cbioportal_importer/.ssh/id_rsa_restful_login_counter_key"
-        RSYNC_RSH="--rsh=ssh -i $RSYNC_IDENTITY_FILE"
-        $CREATE_INDEX_SCRIPT "$ONCOKB_OUT_DIR" "$TMP_DIR" "$BASE_URL" "$TITLE_STRING"
-        rsync -a --rsh="ssh -i $RSYNC_IDENTITY_FILE" "$ONCOKB_OUT_DIR/" "$RSYNC_TARGET"
-        rsync -a --rsh="ssh -i $RSYNC_IDENTITY_FILE" "$INDEX_FILE" "$RSYNC_TARGET"
-        rm -f "$INDEX_FILE"
     }
 
     function dmp_import_run_is_happening() {
@@ -78,6 +53,46 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
         return 1 # completion of import did not happen before timeout
     }
 
+    function gzip_oncokb_annotated_files() {
+        echo $(date)
+        echo "Gzipping data files for OncoKB Annotation for MSKSOLIDHEME"
+        if ! gzip -7 --force $ONCOKB_OUT_DIR/data*.txt ; then
+            message="Error during attempt to gzip $ONCOKB_OUT_DIR data files"
+            send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
+            exit 1
+        fi
+    }
+
+    function rsync_latest_oncokb_annotated_msk_impact_to_clone() {
+        echo $(date)
+        echo "Rsyncing data files to clone of oncokb-annotated-msk-impact git repository"
+        rsync --exclude "/data/portal-cron/git-repos/oncokb-annotated-msk-impact/.git" -a $ONCOKB_OUT_DIR/ $ONCOKB_ANNOTATED_GIT_DIR
+    }
+
+    function commit_and_push_latest_oncokb_annotated_msk_impact() {
+        echo $(date)
+        echo "Committing (amending) and pushing data file changes to git repository"
+        wd=$(pwd)
+        cd $ONCOKB_ANNOTATED_GIT_DIR
+        # Create and push a git amended changeset
+        if ! git add . ; then
+            message="Error during attempt to 'git add .' in repository oncokb-annotated-msk-impact"
+            send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
+            exit 1
+        fi
+        if ! git commit --amend --message "latest oncokb annotations of mskimpact" ; then
+            message="Error occurred during 'git commit --amend' ..."
+            send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
+            exit 1
+        fi
+        if ! git push --force ; then
+            message="Error occurred during 'git push --force'"
+            send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
+            exit 1
+        fi
+        cd $wd
+    }
+
     # retrieve oncokb token from file
     if [ ! -r $ONCOKB_TOKEN_FILE ]; then
         message="Could not run oncokb annotation script: token file '$ONCOKB_TOKEN_FILE' does not exist or is not readable."
@@ -95,6 +110,7 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     SOURCE_DIR="/data/portal-cron/cbio-portal-data/dmp/msk_solid_heme/oncokb"
     STAGING_DIR="/data2/portal-cron/postprocessing/msk_solid_heme"
     ONCOKB_OUT_DIR="$STAGING_DIR/oncokb"
+    ONCOKB_ANNOTATED_GIT_DIR="/data/portal-cron/git-repos/oncokb-annotated-msk-impact"
 
     # scripts
     MAF_ANNOTATOR_SCRIPT="$ONCOKB_ANNOTATOR_HOME/MafAnnotator.py"
@@ -129,10 +145,11 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     ONCOKB_PDF_FILE="$ONCOKB_OUT_DIR/data_clinical_sample.oncokb.pdf"
     ONCOKB_SOMATIC_PDF_FILE="$ONCOKB_OUT_DIR/data_clinical_sample_somatic.oncokb.pdf"
 
-    ONCOKB_README_FILE="$ONCOKB_OUT_DIR/README"
+    ONCOKB_ANNOTATOR_README_FILE="$ONCOKB_OUT_DIR/README"
+    GITHUB_README_FILE="$ONCOKB_OUT_DIR/README.md"
 
     # Make sure necessary environment variables are set before running
-    if [ -z $PYTHON3_BINARY ] || [ -z $GIT_BINARY ] || [ -z $ONCOKB_ANNOTATOR_HOME ] || [ -z $DMP_DATA_HOME ] || [ -z $MSK_SOLID_HEME_DATA_HOME ] || [ -z $ONCOKB_URL ] || [ -z $CANCER_HOTSPOTS_URL ] || [ -z $ONCOKB_TOKEN_FILE ] || [ -z $PORTAL_HOME ] ; then
+    if [ -z $PYTHON3_BINARY ] || [ -z $GIT_BINARY ] || [ -z $ONCOKB_ANNOTATOR_HOME ] || [ -z $DMP_DATA_HOME ] || [ -z $MSK_SOLID_HEME_DATA_HOME ] || [ -z $ONCOKB_TOKEN_FILE ] || [ -z $PORTAL_HOME ] ; then
         message="Could not run oncokb annotation script: automation-environment.sh script must be run in order to set needed environment variables."
         send_failure_messages "$message" "oncokb-annotator failed to run." "$message"
         exit 2
@@ -157,9 +174,9 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     echo "Staring oncokb-annotator run on $SOURCE_DIR"
     ONCOKB_ANNOTATION_SUCCESS=1
 
-    # check if oncokb_annotator clone is up to date and log if not
-    if ! git_clone_is_up_to_date "$ONCOKB_ANNOTATOR_HOME" ; then
-        echo "WARNING : repository clone is out of date at $ONCOKB_ANNOTATOR_HOME"
+    # check if oncokb_annotator code is up to date and log if not
+    if ! oncokb_annotator_code_is_up_to_date ; then
+        echo "WARNING : oncokb_annotator code repository clone is out of date at $ONCOKB_ANNOTATOR_HOME"
     fi
 
     # Pause, up to some time limit for dmp imports to complete
@@ -213,7 +230,7 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
             fi
         fi
 
-        $PYTHON3_BINARY $MAF_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_MAF_FILE -o $ONCOKB_MAF_FILE -c $STAGING_SAMPLE_FILE -u $ONCOKB_URL -v $CANCER_HOTSPOTS_URL -a $PREVIOUS_ANNOTATED_MAF_ARGS
+        $PYTHON3_BINARY $MAF_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_MAF_FILE -o $ONCOKB_MAF_FILE -c $STAGING_SAMPLE_FILE -a $PREVIOUS_ANNOTATED_MAF_ARGS
         if [ $? -ne 0 ] ; then
             echo "Failed to annotate MAF, exiting..."
             ONCOKB_ANNOTATION_SUCCESS=0
@@ -229,7 +246,7 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
         echo $(date)
         echo "Beginning fusions annotation..."
-        $PYTHON3_BINARY $FUSION_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_FUSION_FILE -o $ONCOKB_FUSION_FILE -c $STAGING_SAMPLE_FILE -u $ONCOKB_URL
+        $PYTHON3_BINARY $FUSION_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_FUSION_FILE -o $ONCOKB_FUSION_FILE -c $STAGING_SAMPLE_FILE
         if [ $? -ne 0 ] ; then
             echo "Failed to annotate fusion file, exiting..."
             ONCOKB_ANNOTATION_SUCCESS=0
@@ -240,7 +257,7 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
         echo $(date)
         echo "Beginning CNA annotation..."
-        $PYTHON3_BINARY $CNA_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_CNA_FILE -o $ONCOKB_CNA_FILE -c $STAGING_SAMPLE_FILE -u $ONCOKB_URL
+        $PYTHON3_BINARY $CNA_ANNOTATOR_SCRIPT -b $ONCOKB_TOKEN -i $STAGING_CNA_FILE -o $ONCOKB_CNA_FILE -c $STAGING_SAMPLE_FILE
         if [ $? -ne 0 ] ; then
             echo "Failed to annotate CNA file, exiting..."
             ONCOKB_ANNOTATION_SUCCESS=0
@@ -297,21 +314,28 @@ FLOCK_FILEPATH="/data/portal-cron/cron-lock/oncokb-annotator.sh"
     if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
         echo $(date)
         echo "Beginning README generation..."
-        $PYTHON3_BINARY $README_SCRIPT -o $ONCOKB_README_FILE -u $ONCOKB_URL
+        $PYTHON3_BINARY $README_SCRIPT -o $ONCOKB_ANNOTATOR_README_FILE
         if [ $? -ne 0 ] ; then
             echo "Failed to generate README, exiting..."
             ONCOKB_ANNOTATION_SUCCESS=0
+        else
+            echo '# MSK Clinical Sequencing Cohort (MSKCC)' > $GITHUB_README_FILE
+            echo >> $GITHUB_README_FILE
+            echo 'Please follow the [publication guidelines](http://cmo.mskcc.org/cmo/initiatives/msk-impact/) when using these data in abstracts or journal articles. Manuscripts involving pan-cancer and other large analyses will undergo a biostatistical review prior to submission. If you would like assistance identifying a collaborator in Biostatistics, please contact [Mithat Gonen (gonenm@mskcc.org)](mailto:Mithat%20Gonen%3cgonenm@mskcc.org%3e). For questions regarding the results shown here, please contact [Mike Berger (bergerm1@mskcc.org)](mailto:Mike%20Berger%3cbergerm1@mskcc.org%3e), [Ahmet Zehir (zehira@mskcc.org)](mailto:Ahmet%20Zehir%3czehira@mskcc.org%3e), or [Nikolaus Schultz (schultzn@mskcc.org)](mailto:Nikolaus%20Schultz%3cschultzn@mskcc.org%3e").' >> $GITHUB_README_FILE
+            echo >> $GITHUB_README_FILE
+            echo '### :warning: These data are available to MSK investigators only and are not to be published or shared with anyone outside of MSK without permission.' >> $GITHUB_README_FILE
+            echo >> $GITHUB_README_FILE
+            cat $ONCOKB_ANNOTATOR_README_FILE >> $GITHUB_README_FILE
+            echo >> $GITHUB_README_FILE
+            rm $ONCOKB_ANNOTATOR_README_FILE
         fi
     fi
 
     # Only commit if all steps succeeded
     if [ $ONCOKB_ANNOTATION_SUCCESS -eq 1 ] ; then
-        echo $(date)
-        echo "Gzipping data files for OncoKB Annotation for MSKSOLIDHEME"
         gzip_oncokb_annotated_files
-        echo $(date)
-        echo "Running rsync to download.cbioportal.org for OncoKB Annotation for MSKSOLIDHEME"
-        rsync_oncokb_annotated_files
+        rsync_latest_oncokb_annotated_msk_impact_to_clone
+        commit_and_push_latest_oncokb_annotated_msk_impact
     else
         message="OncoKB Annotation failed for MSKSOLIDHEME. Check logs for more details."
         send_failure_messages "$message" "MSKSOLIDHEME OncoKB Annotation Failure" "MSKSOLIDHEME OncoKB Annotation"
