d655f477dfa52c183d7e67deb9d1d496bfc3efd9
Robert Sheridan <sheridan@cbio.mskcc.org>
2023-01-31 11:12:31 -0500

    filter germline events and all synonyous events
    - uses the unique mutation event key to detect duplicates

--- import-scripts/filter_non_somatic_events_py3.py
+++ import-scripts/filter_non_somatic_events_py3.py
@@ -13,9 +13,49 @@ Example:
 """
 
 import argparse
+import sys
 from enum import Enum
 from generate_az_study_changelog_py3 import DataHandler
 
+UNIQUE_MUTATION_EVENT_KEY_FIELDS = {
+    "Hugo_Symbol",
+    "Chromosome",
+    "Start_Position",
+    "End_Position",
+    "Reference_Allele",
+    "Tumor_Seq_Allele2",
+    "Tumor_Sample_Barcode"
+}
+
+UNIQUE_STRUCTURAL_VARIANT_EVENT_KEY_FIELDS = {
+    "Sample_ID",
+    "Site1_Hugo_Symbol",
+    "Site2_Hugo_Symbol",
+    "Site1_Entrez_Gene_Id",
+    "Site2_Entrez_Gene_Id",
+    "Site1_Region_Number",
+    "Site2_Region_Number",
+    "Site1_Region",
+    "Site2_Region",
+    "Site1_Chromosome",
+    "Site2_Chromosome",
+    "Site1_Contig",
+    "Site2_Contig",
+    "Site1_Position",
+    "Site2_Position",
+    "Event_Info",
+    "Breakpoint_Type",
+    "Connection_Type"
+}
+
+REQUIRED_MUTATION_EVENT_FIELDS =
+    UNIQUE_MUTATION_EVENT_KEY_FIELDS.union({"Mutation_Status"})
+
+REQUIRED_STRUCTURAL_VARIANT_EVENT_FIELDS =
+    UNIQUE_STRUCTURAL_VARIANT_EVENT_KEY_FIELDS.union({"SV_Status"})
+
+ALL_REFERENCED_EVENT_FIELDS =
+    REQUIRED_MUTATION_EVENT_FIELDS.union(REQUIRED_STRUCTURAL_VARIANT_EVENT_FIELDS)
 
 class EventType(Enum):
     """An Enum class to represent mutation or structural variant event types."""
@@ -23,15 +63,27 @@ class EventType(Enum):
     MUTATION = 1
     STRUCTURAL_VARIANT = 2
 
-
 class LineProcessor:
-    """Handles the processing of each line - filtering for somatic events only"""
+    """Functionality common to all line processors"""
 
-    def __init__(self, event_type, col_indices, output_file_handle):
+    def __init__(self, event_type, col_indices):
         self.event_type = event_type
         self.col_indices = col_indices
-        self.output_file_handle = output_file_handle
-        self.header_was_written = False
+        self.raise_exception_if_missing_required_fields()
+
+    def raise_exception_if_missing_required_fields(self):
+        required_field_set = set()
+        if self.event_type == EventType.MUTATION:
+            required_field_set = REQUIRED_MUTATION_EVENT_FIELDS
+        if self.event_type == EventType.STRUCTURAL_VARIANT:
+            required_field_set = REQUIRED_STRUCTURAL_VARIANT_EVENT_FIELDS
+        missing_field_list = {}
+        for field_name in required_field_set:
+            if not field_name in self.col_indices:
+                missing_field_list.add(field_name)
+            if len(missing_field_list) > 0:
+                missing_fields = ",".join(missing_field_list)
+                raise IndexError(f'Unable to find required column(s) {missing_fields} in event file')
 
     def line_is_commented(self, line):
         """Determines if the given line in the file is a comment.
@@ -43,6 +95,70 @@ class LineProcessor:
             boolean: True or False, indicating whether the line is a comment
         """
         return line[0] == '#'
+
+    def convert_line_to_fields(self, line):
+        return line.rstrip("\n").split("\t")
+
+    def convert_line_to_field(self, field_index, line):
+        fields = self.convert_line_to_fields(line)
+        return fields[field_index]
+
+    def compute_key_for_line(self, line):
+        unique_key_field_set = set()
+        if self.event_type == EventType.MUTATION:
+            unique_key_field_set = UNIQUE_MUTATION_EVENT_KEY_FIELDS
+        if self.event_type == EventType.STRUCTURAL_VARIANT:
+            unique_key_field_set = UNIQUE_STRUCTURAL_VARIANT_EVENT_KEY_FIELDS
+        # key will be string representation of the object
+        fields = self.convert_line_to_fields(line)
+        key_value_terms = []
+        for key in sorted(unique_key_field_set):
+            key_value_terms.append(key + "\t" + fields[self.col_indices[key]])
+        computed_keys = "\t".join(computed_keys)
+
+
+class LineGermlineEventScanner(LineProcessor):
+    """Registers the unique event keys for each event with germline status"""
+
+    def __init__(self, event_type, col_indices, germline_event_key_set)
+        super().__init__(event_type, col_indices)
+        self.output_file_handle = output_file_handle
+        self.header_was_seen = False
+        self.germline_event_key_set = germline_event_key_set
+
+    def scan(self, line):
+        """Scan data lines for all events which have status 'GERMLINE" and register them
+
+        Args:
+            line (string): A line from the input file
+
+        Raises:
+            IndexError: If any required column is not found in the input file
+        """
+        if self.line_is_commented(line):
+            return
+        if not self.header_was_seen:
+            self.header_was_seen = True
+            return
+
+        status_col_index = None
+        if event_type == EventType.MUTATION:
+            status_col_index = self.col_indices['Mutation_Status']
+        elif event_type == EventType.STRUCTURAL_VARIANT:
+            status_col_index = self.col_indices['SV_Status']
+        value = self.convert_line_to_field(status_col_index, line)
+        if value.casefold() == 'GERMLINE'.casefold():
+            self.germline_event_key_set.add(self.compute_key_for_line(line))
+
+
+class LineFileWriter(LineProcessor):
+    """Handles the processing of each line - filtering for somatic events only"""
+
+    def __init__(self, event_type, col_indices, germline_event_key_set, output_file_handle):
+        super().__init__(event_type, col_indices)
+        self.output_file_handle = output_file_handle
+        self.header_was_written = False
+        self.germline_event_key_set = germline_event_key_set
 
     def process(self, line):
         """Process each line of the given file to remove all events that are not 'SOMATIC' or 'UNKNOWN'.
@@ -63,21 +179,9 @@ class LineProcessor:
             self.header_was_written = True
             return
 
-        col_index = None
-        if event_type == EventType.MUTATION:
-            if 'Mutation_Status' not in self.col_indices:
-                raise IndexError('Unable to find Mutation_status column in event file')
-            col_index = self.col_indices['Mutation_Status']
-        elif event_type == EventType.STRUCTURAL_VARIANT:
-            if 'SV_Status' not in self.col_indices:
-                raise IndexError('Unable to find SV_Status column in event file')
-            col_index = self.col_indices['SV_Status']
-
-        cols = line.split('\t')
-        value = cols[col_index].rstrip('\n')
-        if value.casefold() == 'SOMATIC'.casefold() or value.casefold() == 'UNKNOWN'.casefold():
+        line_key = self.compute_key_for_line(line)
+        if not line_key in self.germline_event_key_set:
             self.output_file_handle.write(line)
-
 
 class FilteredFileWriter:
     """Handles writing the filtered file containing only somatic events"""
@@ -87,13 +191,20 @@ class FilteredFileWriter:
         self.output_file_path = output_file_path
         self.event_type = event_type
         self.data_handler = DataHandler(input_file_path)
-        self.col_indices = self.data_handler.get_col_indices({"Mutation_Status", "SV_Status"})
+        self.col_indices = self.data_handler.get_col_indices(ALL_REFERENCED_EVENT_FIELDS)
+        self.germline_event_keys = set()
 
     def write(self):
         """Processes the input file and writes out a filtered version including only somatic events"""
+        # scan for all germline events and record keys
+        with open(input_file_path, "r") as input_file_handle:
+            line_germline_event_scanner = LineGermlineEventScanner(self.event_type, self.col_indices, self.germline_event_keys)
+            for line in input_file_handle:
+                line_germline_event_scanner.scan(line)
+        # read/write events, filtering those which match a germline event key
         with open(input_file_path, "r") as input_file_handle:
             with open(output_file_path, "w") as output_file_handle:
-                line_processor = LineProcessor(self.event_type, self.col_indices, output_file_handle)
+                line_processor = LineFileWriter(self.event_type, self.col_indices, self.germline_event_keys, output_file_handle)
                 for line in input_file_handle:
                     line_processor.process(line)
 
